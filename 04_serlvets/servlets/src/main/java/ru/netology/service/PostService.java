48df09eba817feec98be510a0e9b5a2519f9a162
RVSN451 <kvg.huawei@gmail.com>
2021-09-03 16:21:06 +0300

    HW-servlet

--- 04_serlvets/servlets/src/main/java/ru/netology/service/PostService.java
+++ 04_serlvets/servlets/src/main/java/ru/netology/service/PostService.java
@@ -4,29 +4,30 @@ import ru.netology.exception.NotFoundException;
 import ru.netology.model.Post;
 import ru.netology.repository.PostRepository;
 
-import java.util.List;
+import java.util.Map;
 
 public class PostService {
-  private final PostRepository repository;
+    private final PostRepository repository;
 
-  public PostService(PostRepository repository) {
-    this.repository = repository;
-  }
+    public PostService(PostRepository repository) {
+        this.repository = repository;
+    }
 
-  public List<Post> all() {
-    return repository.all();
-  }
+    public Map<Long, Post> all() {
+        repository.all().clear();
+        return repository.all();
+    }
 
-  public Post getById(long id) {
-    return repository.getById(id).orElseThrow(NotFoundException::new);
-  }
+    public Post getById(long id) {
+        return repository.getById(id).orElseThrow(NotFoundException::new);
+    }
 
-  public Post save(Post post) {
-    return repository.save(post);
-  }
+    public Post save(Post post) {
+        return repository.save(post).orElseThrow(NotFoundException::new);
+    }
 
-  public void removeById(long id) {
-    repository.removeById(id);
-  }
+    public void removeById(long id) {
+        repository.removeById(id);
+    }
 }
 
