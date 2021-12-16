710416c8cb7f09d772bf45226128cd9ce3bcaa79
Olga Morozova <Morozova_Olga_22@mail.ru>
2021-12-16 19:15:53 +0300

    Repository updated +Refactoring

--- 04_serlvets/servlets/src/main/java/ru/netology/controller/PostController.java
+++ 04_serlvets/servlets/src/main/java/ru/netology/controller/PostController.java
@@ -7,35 +7,47 @@ import ru.netology.service.PostService;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.Reader;
+import java.util.List;
 
 public class PostController {
-  public static final String APPLICATION_JSON = "application/json";
-  private final PostService service;
+    public static final String APPLICATION_JSON = "application/json";
+    private final PostService service;
 
-  public PostController(PostService service) {
-    this.service = service;
-  }
+    public PostController(PostService service) {
+        this.service = service;
+    }
 
-  public void all(HttpServletResponse response) throws IOException {
-    response.setContentType(APPLICATION_JSON);
-    final var data = service.all();
-    final var gson = new Gson();
-    response.getWriter().print(gson.toJson(data));
-  }
+    public void all(HttpServletResponse response) throws IOException {
+        final ResponseHandler<List<Post>> handler = service::all;
+        makeResponse(handler, response);
+    }
 
-  public void getById(long id, HttpServletResponse response) {
-    // TODO: deserialize request & serialize response
-  }
+    public void getById(long id, HttpServletResponse response) throws IOException {
+        final ResponseHandler<Post> handler = () -> service.getById(id);
+        makeResponse(handler, response);
+    }
 
-  public void save(Reader body, HttpServletResponse response) throws IOException {
-    response.setContentType(APPLICATION_JSON);
-    final var gson = new Gson();
-    final var post = gson.fromJson(body, Post.class);
-    final var data = service.save(post);
-    response.getWriter().print(gson.toJson(data));
-  }
+    public void save(Reader body, HttpServletResponse response) throws IOException {
+        final var gson = new Gson();
+        final var post = gson.fromJson(body, Post.class);
+        final ResponseHandler<Post> handler = () -> service.save(post);
+        makeResponse(handler, response);
+    }
 
-  public void removeById(long id, HttpServletResponse response) {
-    // TODO: deserialize request & serialize response
-  }
+    public void removeById(long id, HttpServletResponse response) {
+        service.removeById(id);
+        response.setStatus(HttpServletResponse.SC_OK);
+    }
+
+    private <T> void makeResponse(ResponseHandler<T> handler, HttpServletResponse response) throws IOException {
+        response.setContentType(APPLICATION_JSON);
+        final var data = handler.makeResponseData();
+        final var gson = new Gson();
+        response.getWriter().print(gson.toJson(data));
+    }
+
+    @FunctionalInterface
+    interface ResponseHandler<T> {
+        T makeResponseData();
+    }
 }
--- 04_serlvets/servlets/src/main/java/ru/netology/service/PostService.java
+++ 04_serlvets/servlets/src/main/java/ru/netology/service/PostService.java
@@ -7,26 +7,26 @@ import ru.netology.repository.PostRepository;
 import java.util.List;
 
 public class PostService {
-  private final PostRepository repository;
+    private final PostRepository repository;
 
-  public PostService(PostRepository repository) {
-    this.repository = repository;
-  }
+    public PostService(PostRepository repository) {
+        this.repository = repository;
+    }
 
-  public List<Post> all() {
-    return repository.all();
-  }
+    public List<Post> all() {
+        return repository.all();
+    }
 
-  public Post getById(long id) {
-    return repository.getById(id).orElseThrow(NotFoundException::new);
-  }
+    public Post getById(long id) {
+        return repository.getById(id).orElseThrow(NotFoundException::new);
+    }
 
-  public Post save(Post post) {
-    return repository.save(post);
-  }
+    public Post save(Post post) {
+        return repository.save(post);
+    }
 
-  public void removeById(long id) {
-    repository.removeById(id);
-  }
+    public void removeById(long id) {
+        repository.removeById(id);
+    }
 }
 
