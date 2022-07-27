2ef8213233cbbee039b845c4a53bd4ba995b38fe
LarissaAbr2801 <abramova.la.28@gmail.com>
2022-07-27 13:09:02 +0300

    added thread safety and exceptions

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -6,20 +6,21 @@ import ru.netology.model.Post;
 
 import java.nio.charset.StandardCharsets;
 import java.util.*;
-import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 // Stub
 public class PostRepository {
 
-    private final List<Post> posts = new CopyOnWriteArrayList<>();
-    private int postsCounter = 0;
+    private final Map<Long, Post> posts = new ConcurrentHashMap<>();
+    private final AtomicInteger postsCounter = new AtomicInteger(0);
 
     public List<Post> all() {
-        return posts;
+        return (List<Post>) posts.values();
     }
 
     public Optional<Post> getById(long id) {
-        return posts.stream()
+        return posts.values().stream()
                 .filter(x -> x.getId() == id)
                 .findAny();
     }
@@ -30,13 +31,13 @@ public class PostRepository {
             var postInList = getById(post.getId());
 
             if (postInList.isPresent()) {
-                posts.set(posts.indexOf(postInList.get()), post);
+                posts.put(postInList.get().getId(), post);
             } else {
                 throw new NotFoundException("Невозможно сохранить пост!");
             }
         } else {
-            post.setId(postsCounter++);
-            posts.add(post);
+            post.setId(postsCounter.addAndGet(1));
+            posts.put(post.getId(), post);
         }
         post.setContent(String.valueOf(URLEncodedUtils.parse(post.getContent(),
                 StandardCharsets.UTF_8).get(0)));
@@ -44,6 +45,6 @@ public class PostRepository {
     }
 
     public void removeById(long id) {
-        posts.removeIf(post -> post.getId() == id);
+        posts.values().removeIf(post -> post.getId() == id);
     }
 }
--- src/main/java/ru/netology/servlet/MainServlet.java
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -1,6 +1,7 @@
 package ru.netology.servlet;
 
 import ru.netology.controller.PostController;
+import ru.netology.exception.NotFoundException;
 import ru.netology.repository.PostRepository;
 import ru.netology.service.PostService;
 
@@ -50,6 +51,9 @@ public class MainServlet extends HttpServlet {
                 controller.removeById(id, resp);
                 return;
             }
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        } catch (NotFoundException e) {
+            e.printStackTrace();
             resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
         } catch (Exception e) {
             e.printStackTrace();
