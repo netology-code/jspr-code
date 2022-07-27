fb14c03410dc820ae23440e72ceaefedbb8af7ce
LarissaAbr2801 <abramova.la.28@gmail.com>
2022-07-27 13:43:49 +0300

    spring Annotation Config

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -1,6 +1,7 @@
 package ru.netology.repository;
 
 import org.apache.http.client.utils.URLEncodedUtils;
+import org.springframework.stereotype.Controller;
 import ru.netology.exception.NotFoundException;
 import ru.netology.model.Post;
 
@@ -10,6 +11,8 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
 // Stub
+
+@Controller
 public class PostRepository {
 
     private final Map<Long, Post> posts = new ConcurrentHashMap<>();
