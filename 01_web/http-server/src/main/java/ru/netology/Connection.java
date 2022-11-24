521930d931bd795bf2de6381de8bb840ec60c05f
sezergemtsov <sezer.gemtsov@ya.ru>
2022-11-24 12:52:20 +0300

    reworking Request fields 1.0

--- 01_web/http-server/src/main/java/ru/netology/Connection.java
+++ 01_web/http-server/src/main/java/ru/netology/Connection.java
@@ -1,5 +1,8 @@
 package ru.netology;
 
+import org.apache.hc.core5.http.NameValuePair;
+import org.apache.hc.core5.http.message.BasicNameValuePair;
+
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -7,19 +10,19 @@ import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDateTime;
-import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Callable;
+import java.util.stream.Stream;
 
 public class Connection implements Callable<Boolean> {
 
     private final Server server;
-    private Request request;
 
     protected Connection(Server server) {
         this.server = server;
     }
-
 
     @Override
     public Boolean call() throws Exception {
@@ -31,39 +34,25 @@ public class Connection implements Callable<Boolean> {
 
             final var limit = 4096;
             final var buffer = new char[limit];
-            final var requestBytes = in.readLine();
-            System.out.println(requestBytes);
 
-            for (int i = 0; i < limit; i++) {
-                if (in.ready()) {
-                    in.read(buffer);
-                } else {
-                    break;
-                }
-            }
+            in.read(buffer,0,limit);
 
-            final var parts = requestBytes.split(" ");
+            Request request = new Request();
+            setIsCorrect(buffer,limit,request);
+            request.setRequestHeaders(setHeaders(buffer));
 
-            request = new Request();
-            request.setBuffer(buffer);
-
-            if (parts.length == 3) {
-                request.setRequestLine(parts[1]).setRequestMethod(parts[0]);
-            } else {
+            if (!request.getIsCorrect()) {
                 badRequest(out);
-                return false;
             }
-
-            request.getHeaders().stream().forEach(System.out::println);
 
             if (!server.getHandlers().isEmpty()) {
-                Iterator iterator = server.getHandlers().entrySet().iterator();
+                var iterator = server.getHandlers().entrySet().iterator();
                 while (iterator.hasNext()) {
-                    Map.Entry<String, Map<String, Handler>> pair = (Map.Entry) iterator.next();
+                    Map.Entry<String, Map<String, Handler>> pair = iterator.next();
                     if (request.getRequestMethod().equals(pair.getKey())) {
-                        Iterator iterator1 = pair.getValue().entrySet().iterator();
+                        var iterator1 = pair.getValue().entrySet().iterator();
                         while (iterator1.hasNext()) {
-                            Map.Entry<String, Handler> map1 = (Map.Entry) iterator1.next();
+                            Map.Entry<String, Handler> map1 = iterator1.next();
                             if (map1.getKey().equals(request.getPath())) {
                                 map1.getValue().handle(request, out);
                                 return false;
@@ -73,10 +62,14 @@ public class Connection implements Callable<Boolean> {
                 }
             }
 
-            final var path = request.getRequestLine();
-            if (!server.getValidPaths().contains(path)) {
+            final var path = request.getPath();
+            if (path == null) {
                 badRequest(out);
-                return false;
+            } else {
+                if (!server.getValidPaths().contains(path)) {
+                    badRequest(out);
+                    return false;
+                }
             }
 
             final var filePath = Path.of(".", "public", path);
@@ -124,4 +117,53 @@ public class Connection implements Callable<Boolean> {
         ).getBytes());
         out.flush();
     }
+
+    private static int indexOf(char[] array, char[] target, int start, int max) {
+        outer:
+        for (int i = start; i < max - target.length + 1; i++) {
+            for (int j = 0; j < target.length; j++) {
+                if (array[i + j] != target[j]) {
+                    continue outer;
+                }
+            }
+            return i;
+        }
+        return -1;
+    }
+
+    public void setIsCorrect(char[] buffer, int limit, Request request) {
+        char[] target = new char[]{'\r','\n'};
+        int index = indexOf(buffer, target, 0 , limit);
+        String[] firstLine = String.valueOf(buffer).substring(0, index).split(" ");
+        if (firstLine.length != 3) {
+            request.setIsCorrect(false);
+        } else {
+            request.setIsCorrect(true);
+            request.setRequestMethod(firstLine[0]);
+            if (firstLine[1].contains("?")) {
+                request.setRequestPath(firstLine[1].substring(0, firstLine[1].indexOf("?")));
+                request.setQueryLine(firstLine[1].substring(firstLine[1].indexOf("?"), firstLine[1].length() - 1));
+                request.setIsQuery(true);
+            } else {
+                request.setRequestPath(firstLine[1]);
+                request.setIsQuery(false);
+            }
+        }
+    }
+
+    protected List<NameValuePair> setHeaders(char[] buffer) {
+        char[] targetF = new char[]{'\r', '\n', '\r', '\n'};
+        char[] targetS = new char[]{'\r', '\n'};
+        int startOfHeaders = indexOf(buffer,targetS,0,buffer.length)+2;
+        int finishOfHeaders = indexOf(buffer,targetF,startOfHeaders,buffer.length);
+        List<NameValuePair> requestHeaders = new ArrayList<>();
+        String[] headersValues = String.valueOf(buffer).substring(startOfHeaders,finishOfHeaders).split("\r\n");
+        Stream.of(headersValues)
+                .forEach(x->{
+                    String[] strgs = x.split(":");
+                    requestHeaders.add(new BasicNameValuePair(strgs[0],strgs[1]));
+                });
+        return requestHeaders;
+    }
+
 }
--- 01_web/http-server/src/main/java/ru/netology/Request.java
+++ 01_web/http-server/src/main/java/ru/netology/Request.java
@@ -1,74 +1,39 @@
 package ru.netology;
 
 import org.apache.hc.core5.http.NameValuePair;
-import org.apache.hc.core5.http.message.BasicNameValuePair;
-import org.apache.hc.core5.net.URLEncodedUtils;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
 
 public class Request {
+    private boolean isCorrect;
     private String requestMethod;
-    private String requestLine;
-    private String requestBody;
     private String requestPath;
+    private String queryLine;
+    private boolean isQuery;
     private List<NameValuePair> queryParams;
-    private char[] buffer;
     private List<NameValuePair> requestHeaders;
+    private String requestBody;
     private List<NameValuePair> postParams;
 
-    protected Request() {
-    }
-
-    public void setBuffer(char[] buffer) {
-        this.buffer = buffer;
-    }
-
-    public char[] getBuffer() {
-        return buffer;
-    }
+    protected Request() {}
 
     public String getRequestMethod() {
         return requestMethod;
     }
+    public String getPath() { return requestPath; }
+    public boolean getIsCorrect() { return isCorrect; }
 
-    protected Request setRequestMethod(String requestMethod) {
-        this.requestMethod = requestMethod;
-        return this;
-    }
+    public void setIsCorrect(boolean bool) {this.isCorrect = bool;}
+    public void setRequestMethod(String method){this.requestMethod = method;}
+    public void setRequestPath(String requestPath){this.requestPath = requestPath;}
+    public void setQueryLine(String queryLine){this.queryLine = queryLine;}
+    public void setIsQuery(boolean isQuery){this.isQuery = isQuery;}
+    public void setRequestHeaders(List<NameValuePair> requestHeaders){this.requestHeaders = requestHeaders;}
 
-    public String getRequestLine() {
-        return requestLine;
-    }
-
-    protected Request setRequestLine(String requestLine) {
-        this.requestLine = requestLine;
-        setPath();
-        return this;
-    }
-
-    public String getRequestBody() {
-        return requestBody;
-    }
-
-    protected Request setRequestBody(String requestBody) {
-        this.requestBody = requestBody;
-        return this;
-    }
-
-    protected List<NameValuePair> getQueryParams() {
-        try {
-            queryParams = URLEncodedUtils.parse( new URI(requestLine) , StandardCharsets.UTF_8);
-            return queryParams;
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
+    protected List<NameValuePair> getQueryParams() { return queryParams; }
+    protected List<NameValuePair> getHeaders() { return requestHeaders; }
+    protected List<NameValuePair> getPostParams() { return postParams;}
 
     protected List<NameValuePair> getQueryParam(String paramName) {
         if (!queryParams.isEmpty()) {
@@ -81,55 +46,24 @@ public class Request {
         return null;
     }
 
-    private String setPath() {
-        if (!requestLine.isEmpty()& requestLine.contains("?")) {
-            requestPath = requestLine.substring(0, requestLine.indexOf("?"));
-            return requestPath;
-        } else {
-            return null;
-        }
-    }
-
-    protected String getPath() {
-        return requestPath;
-    }
-
-    protected List<NameValuePair> getHeaders() {
-            char[] targetF = new char[]{'\r', '\n', '\r', '\n'};
-            char[] targetS = new char[]{'\r', '\n'};
-            int startOfBody = indexOf(buffer,targetS,0,buffer.length)+2;
-            int finishOfBody = indexOf(buffer,targetF,startOfBody,buffer.length);
-            requestHeaders = new ArrayList<>();
-            String[] bodyValues = String.valueOf(buffer).substring(startOfBody,finishOfBody).split("\r\n");
-            Stream.of(bodyValues)
-                    .forEach(x->{
-                       String[] strgs = x.split(":");
-                       requestHeaders.add(new BasicNameValuePair(strgs[0],strgs[1]));
-                    });
-            return requestHeaders;
-    }
-
     protected List<NameValuePair> getHeader(String headerName) {
         if (!requestHeaders.isEmpty()) {
-            List<NameValuePair> postParam = new ArrayList<>();
-            postParam.stream()
-                    .filter(x->x.getName().equals(headerName))
-                    .forEach(x->postParam.add(x));
+            List<NameValuePair> header = new ArrayList<>();
+            requestHeaders.stream()
+                    .filter(x->x.getName().trim().equals(headerName))
+                    .forEach(x->header.add(x));
         }
         return null;
     }
 
-    private static int indexOf(char[] array, char[] target, int start, int max) {
-        outer:
-        for (int i = start; i < max - target.length + 1; i++) {
-            for (int j = 0; j < target.length; j++) {
-                if (array[i + j] != target[j]) {
-                    continue outer;
-                }
-            }
-            return i;
+    protected List<NameValuePair> getPostParam(String postParamName) {
+        if (!postParams.isEmpty()) {
+            List<NameValuePair> postParam = new ArrayList<>();
+            postParams.stream()
+                    .filter(x->x.getName().trim().equals(postParamName))
+                    .forEach(x->postParam.add(x));
         }
-        return -1;
+        return null;
     }
 
 }
