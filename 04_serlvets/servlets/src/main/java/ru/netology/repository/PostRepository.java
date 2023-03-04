8b44c07fbe43b96e22232ba132875cd870225299
Pivovarov Maksim <Scarp55>
2023-03-05 02:01:40 +0600

    di-annotation commit

--- 04_serlvets/servlets/src/main/java/ru/netology/repository/PostRepository.java
+++ 04_serlvets/servlets/src/main/java/ru/netology/repository/PostRepository.java
@@ -1,12 +1,16 @@
 package ru.netology.repository;
 
+import org.springframework.stereotype.Repository;
+import ru.netology.exception.NotFoundException;
 import ru.netology.model.Post;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
 
-// Stub
+@Repository
 public class PostRepository {
 
     private final AtomicLong id = new AtomicLong(0);
