ed84b69264201c64a00db028bc3881bbb51eac46
Tim <book9902@yandex.ru>
2023-05-22 21:47:25 +0300

    Add Logic Repository

--- 04_serlvets/servlets/src/main/java/ru/netology/repository/PostRepository.java
+++ 04_serlvets/servlets/src/main/java/ru/netology/repository/PostRepository.java
@@ -1,25 +1,40 @@
 package ru.netology.repository;
 
+import ru.netology.exception.NotFoundException;
 import ru.netology.model.Post;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
 
 // Stub
 public class PostRepository {
+  private final AtomicLong idPost = new AtomicLong();
+  private final Map<Long, Post> postMap = new ConcurrentHashMap<>();
   public List<Post> all() {
-    return Collections.emptyList();
+    return new ArrayList<>(postMap.values());
   }
 
   public Optional<Post> getById(long id) {
-    return Optional.empty();
+    return Optional.ofNullable(postMap.get(id));
   }
 
   public Post save(Post post) {
+    if (post == null && post.getId()!=0){
+      throw new NotFoundException();
+    }
+    if (post.getId() == 0){
+      var id = idPost.incrementAndGet();
+      post.setId(id);
+    }
+    postMap.put(post.getId(), post);
     return post;
   }
 
   public void removeById(long id) {
+    if (!postMap.containsKey(id)) {
+      throw new NotFoundException();
+    }
+    postMap.remove(id);
   }
 }
