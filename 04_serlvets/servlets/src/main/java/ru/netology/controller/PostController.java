28b9db5d39ef433c94315a3f2c5588cafe6bf7ab
Mikhail Ignatov <mikhail.ignatov@inbox.ru>
2023-06-13 13:09:54 +0300

    2.1. Servlets Containers

--- 04_serlvets/servlets/src/main/java/ru/netology/controller/PostController.java
+++ 04_serlvets/servlets/src/main/java/ru/netology/controller/PostController.java
@@ -1,6 +1,7 @@
 package ru.netology.controller;
 
 import com.google.gson.Gson;
+import ru.netology.exception.NotFoundException;
 import ru.netology.model.Post;
 import ru.netology.service.PostService;
 
@@ -11,31 +12,38 @@ import java.io.Reader;
 public class PostController {
   public static final String APPLICATION_JSON = "application/json";
   private final PostService service;
+  private final Gson gson;
 
   public PostController(PostService service) {
     this.service = service;
+    gson = new Gson();
   }
 
   public void all(HttpServletResponse response) throws IOException {
     response.setContentType(APPLICATION_JSON);
     final var data = service.all();
-    final var gson = new Gson();
+    //final var gson = new Gson();
     response.getWriter().print(gson.toJson(data));
   }
 
-  public void getById(long id, HttpServletResponse response) {
-    // TODO: deserialize request & serialize response
+  public void getById(long id, HttpServletResponse response) throws IOException, NotFoundException {
+    response.setContentType(APPLICATION_JSON);
+    final var post = service.getById(id);
+    response.getWriter().println(gson.toJson(post));
   }
 
   public void save(Reader body, HttpServletResponse response) throws IOException {
     response.setContentType(APPLICATION_JSON);
-    final var gson = new Gson();
+    //final var gson = new Gson();
     final var post = gson.fromJson(body, Post.class);
     final var data = service.save(post);
     response.getWriter().print(gson.toJson(data));
   }
 
-  public void removeById(long id, HttpServletResponse response) {
-    // TODO: deserialize request & serialize response
+  public void removeById(long id, HttpServletResponse response) throws IOException {
+    response.setContentType(APPLICATION_JSON);
+    final var data = service.getById(id);
+    service.removeById(id);
+    response.getWriter().print(gson.toJson(data));
   }
 }
--- 04_serlvets/servlets/src/main/java/ru/netology/servlet/MainServlet.java
+++ 04_serlvets/servlets/src/main/java/ru/netology/servlet/MainServlet.java
@@ -9,6 +9,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class MainServlet extends HttpServlet {
+  public static final String apiPosts = "/api/posts";
+  public static final String apiPostsD = "/api/posts/\\d+";
+  public static final String str = "/";
+  public static final String getMethod = "GET";
+  public static final String postMethod = "POST";
+  public static final String deleteMethod = "DELETE";
   private PostController controller;
 
   @Override
@@ -25,23 +31,23 @@ public class MainServlet extends HttpServlet {
       final var path = req.getRequestURI();
       final var method = req.getMethod();
       // primitive routing
-      if (method.equals("GET") && path.equals("/api/posts")) {
+      if (method.equals(getMethod) && path.equals(apiPosts)) {
         controller.all(resp);
         return;
       }
-      if (method.equals("GET") && path.matches("/api/posts/\\d+")) {
+      if (method.equals(getMethod) && path.matches(apiPostsD)) {
         // easy way
-        final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
+        final var id = parseId(path);
         controller.getById(id, resp);
         return;
       }
-      if (method.equals("POST") && path.equals("/api/posts")) {
+      if (method.equals(postMethod) && path.equals(apiPosts)) {
         controller.save(req.getReader(), resp);
         return;
       }
-      if (method.equals("DELETE") && path.matches("/api/posts/\\d+")) {
+      if (method.equals(deleteMethod) && path.matches(apiPostsD)) {
         // easy way
-        final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
+        final var id = parseId(path);
         controller.removeById(id, resp);
         return;
       }
@@ -50,6 +56,10 @@ public class MainServlet extends HttpServlet {
       e.printStackTrace();
       resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
     }
+  }
+
+  private long parseId(String path) {
+    return Long.parseLong(path.substring(path.lastIndexOf(str)));
   }
 }
 
