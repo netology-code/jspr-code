6aa743c87b7b63e8d1dd204d8a3f5de430a92a07
AndreyAndrianov jdplm-32 <darkc0lder@mail.ru>
2023-07-17 23:31:22 +0300

    di commit

--- 04_serlvets/servlets/src/main/java/ru/netology/servlet/MainServlet.java
+++ 04_serlvets/servlets/src/main/java/ru/netology/servlet/MainServlet.java
@@ -1,5 +1,7 @@
 package ru.netology.servlet;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.netology.config.JavaConfig;
 import ru.netology.controller.PostController;
 import ru.netology.repository.PostRepository;
 import ru.netology.service.PostService;
@@ -16,9 +18,9 @@ public class MainServlet extends HttpServlet {
 
   @Override
   public void init() {
-    PostRepository repository = new PostRepository();
-    PostService service = new PostService(repository);
-    controller = new PostController(service);
+    final var context = new AnnotationConfigApplicationContext(JavaConfig.class);
+    final var factory = context.getBeanFactory();
+    controller = factory.getBean(PostController.class);
   }
 
   @Override
