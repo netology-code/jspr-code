5da7a12dacda02921a419ee061e2516cb8eecb77
Aleksandr <sergeeval.0702@gmail.com>
2024-02-15 18:20:16 +0300

    feature/di-annotation

---
+++ 04_serlvets/servlets/src/main/java/ru/netology/config/AppConfig.java
@@ -0,0 +1,9 @@
+package ru.netology.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "ru.netology")
+public class AppConfig {
+}
--- 04_serlvets/servlets/src/main/java/ru/netology/servlet/MainServlet.java
+++ 04_serlvets/servlets/src/main/java/ru/netology/servlet/MainServlet.java
@@ -1,50 +1,49 @@
 package ru.netology.servlet;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.netology.config.AppConfig;
 import ru.netology.controller.PostController;
-import ru.netology.repository.PostRepositoryImpl;
-import ru.netology.service.PostService;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class MainServlet extends HttpServlet {
-  private static final String POSTS_ENDPOINT = "/api/posts";
-  private static final String POSTS_WITH_ID_ENDPOINT = "/api/posts/";
-
   private PostController controller;
 
   @Override
   public void init() {
-    final var repository = new PostRepositoryImpl();
-    final var service = new PostService(repository);
-    controller = new PostController(service);
+    ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    controller = context.getBean(PostController.class);
   }
 
   @Override
-  protected void service(HttpServletRequest req, HttpServletResponse resp) {
+  protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
     try {
-      final var path = req.getRequestURI();
-      final var method = req.getMethod();
+      final String path = req.getRequestURI();
+      final String method = req.getMethod();
 
-      if (method.equals("GET") && path.equals(POSTS_ENDPOINT)) {
+      if (method.equals("GET") && path.equals("/api/posts")) {
         controller.all(resp);
         return;
       }
 
-      if (method.equals("GET") && path.startsWith(POSTS_WITH_ID_ENDPOINT)) {
-        final var id = Long.parseLong(path.substring(POSTS_WITH_ID_ENDPOINT.length()));
+      if (method.equals("GET") && path.startsWith("/api/posts/")) {
+        final long id = Long.parseLong(path.substring("/api/posts/".length()));
         controller.getById(id, resp);
         return;
       }
 
-      if (method.equals("POST") && path.equals(POSTS_ENDPOINT)) {
+      if (method.equals("POST") && path.equals("/api/posts")) {
         controller.save(req.getReader(), resp);
         return;
       }
 
-      if (method.equals("DELETE") && path.startsWith(POSTS_WITH_ID_ENDPOINT)) {
-        final var id = Long.parseLong(path.substring(POSTS_WITH_ID_ENDPOINT.length()));
+      if (method.equals("DELETE") && path.startsWith("/api/posts/")) {
+        final long id = Long.parseLong(path.substring("/api/posts/".length()));
         controller.removeById(id, resp);
         return;
       }
