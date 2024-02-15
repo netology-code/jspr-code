2223ffc4045c4d9c00e8440251d1f3b0a09b8c1f
Aleksandr <sergeeval.0702@gmail.com>
2024-02-15 18:32:10 +0300

    feature/di-java

--- 04_serlvets/servlets/src/main/java/ru/netology/servlet/MainServlet.java
+++ 04_serlvets/servlets/src/main/java/ru/netology/servlet/MainServlet.java
@@ -11,47 +11,47 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class MainServlet extends HttpServlet {
-  private PostController controller;
+    private PostController controller;
 
-  @Override
-  public void init() {
-    ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+    @Override
+    public void init() {
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 
-    controller = context.getBean(PostController.class);
-  }
-
-  @Override
-  protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-    try {
-      final String path = req.getRequestURI();
-      final String method = req.getMethod();
-
-      if (method.equals("GET") && path.equals("/api/posts")) {
-        controller.all(resp);
-        return;
-      }
-
-      if (method.equals("GET") && path.startsWith("/api/posts/")) {
-        final long id = Long.parseLong(path.substring("/api/posts/".length()));
-        controller.getById(id, resp);
-        return;
-      }
-
-      if (method.equals("POST") && path.equals("/api/posts")) {
-        controller.save(req.getReader(), resp);
-        return;
-      }
-
-      if (method.equals("DELETE") && path.startsWith("/api/posts/")) {
-        final long id = Long.parseLong(path.substring("/api/posts/".length()));
-        controller.removeById(id, resp);
-        return;
-      }
-
-      resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
-    } catch (Exception e) {
-      e.printStackTrace();
-      resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        controller = context.getBean(PostController.class);
     }
-  }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        try {
+            final String path = req.getRequestURI();
+            final String method = req.getMethod();
+
+            if (method.equals("GET") && path.equals("/api/posts")) {
+                controller.all(resp);
+                return;
+            }
+
+            if (method.equals("GET") && path.startsWith("/api/posts/")) {
+                final long id = Long.parseLong(path.substring("/api/posts/".length()));
+                controller.getById(id, resp);
+                return;
+            }
+
+            if (method.equals("POST") && path.equals("/api/posts")) {
+                controller.save(req.getReader(), resp);
+                return;
+            }
+
+            if (method.equals("DELETE") && path.startsWith("/api/posts/")) {
+                final long id = Long.parseLong(path.substring("/api/posts/".length()));
+                controller.removeById(id, resp);
+                return;
+            }
+
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        } catch (Exception e) {
+            e.printStackTrace();
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+    }
 }
